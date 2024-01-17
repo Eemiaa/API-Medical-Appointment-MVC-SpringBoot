4cb0d6b01cf5e658bb76fbc3ad34a7b46e8f1c07
Eemiaa <aimeemiranribe@gmail.com>
2024-01-17 12:58:21 -0300

    deu certo o uso de handlers

--- consulta/src/main/java/com/ConsultaMedica/consulta/Controllers/SessionController.java
+++ consulta/src/main/java/com/ConsultaMedica/consulta/Controllers/SessionController.java
@@ -11,8 +11,10 @@ import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+@Validated
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/session")
@@ -22,21 +24,20 @@ public class SessionController {
         this.sessionService = sessionService;
     }
     @PostMapping("/create/medico")
-    public ResponseEntity<Object> saveMedico(@RequestBody @Valid MedicoDto medicoDto){
-        //colocar handle aqui
+    public ResponseEntity<Object> saveMedico(@Valid @RequestBody MedicoDto medicoDto){
         var medicoModel = new MedicoModel();
         BeanUtils.copyProperties(medicoDto, medicoModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(sessionService.createMedico(medicoModel));
     }
     @PostMapping("/create/paciente")
-    public ResponseEntity<Object> savePaciente(@RequestBody @Valid PacienteDto pacienteDto){
+    public ResponseEntity<Object> savePaciente(@Valid @RequestBody PacienteDto pacienteDto){
         //colocar handle aqui
         var pacienteModel = new PacienteModel();
         BeanUtils.copyProperties(pacienteDto, pacienteModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(sessionService.createPaciente(pacienteModel));
     }
     @PostMapping("/create/secretaria")
-    public ResponseEntity<Object> saveSecretaria(@RequestBody @Valid SecretariaDto secretariaDto){
+    public ResponseEntity<Object> saveSecretaria(@Valid @RequestBody SecretariaDto secretariaDto){
         //colocar handle aqui
         var secretariaModel = new SecretariaModel();
         BeanUtils.copyProperties(secretariaDto, secretariaModel);
--- consulta/src/main/java/com/ConsultaMedica/consulta/Dtos/MedicoDto.java
+++ consulta/src/main/java/com/ConsultaMedica/consulta/Dtos/MedicoDto.java
@@ -6,27 +6,29 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Getter
 @Setter
 public class MedicoDto {
-    @NotBlank
-    @Size(max = 45)
+    @NotBlank(message = "Campo nome não pode ser nulo.")
+    @Size(max = 45, message = "O tamanho ultrapassa o máximo de {max} caracteres.")
     @JsonProperty("nome")
     private String nome;
 
-    @NotBlank
-    @Size(max = 8)
+    @NotBlank(message = "Campo cep não pode ser nulo.")
+    @Size(max = 8, message = "O tamanho ultrapassa o máximo de {max} caracteres.")
     @JsonProperty("cep")
     private String cep;
 
-    @NotBlank
-    @Size(max = 13)
+    @NotBlank(message = "Campo telefone não pode ser nulo.")
+    @Size(max = 13, message = "O tamanho ultrapassa o máximo de {max} caracteres.")
     @JsonProperty("telefone")
     private String telefone;
 
-    @NotBlank
-    @Size(max = 45)
+    @NotBlank(message = "Campo crm não pode ser nulo.")
+    @Size(max = 45,  message = "O tamanho ultrapassa o máximo de {max} caracteres.")
     @JsonProperty("crm")
     private String crm;
 }
